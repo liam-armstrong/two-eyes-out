432b246b9cad1b481ce1f00f85658be1b5d52594
liam-armstrong <liam.armstrong124@gmail.com>
2018-08-15 16:24:56 -0400

    Added actions and reducer for Section List

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,10 +1,22 @@
 import { combineReducers } from 'redux';
 import * as actionType from '../actions/types'
 
+//Token initial state and reducer
 const tokenInitialState = null;
 const token = (state = tokenInitialState, action) => {
     switch(action.type) {
         case actionType.SET_TOKEN:
+            return action.data
+        default:
+            return state;
+    }
+}
+
+//Section list initial state and reducer
+const slistInitialState = [];
+const slist = (state = slistInitialState, action) => {
+    switch(action.type) {
+        case actionType.GET_SECTIONS:
             return action.data
         default:
             return state;
