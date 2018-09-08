2016069f1619c50ca4cc3ca864a56ede580d7d38
liam-armstrong <liam.armstrong124@gmail.com>
2018-09-07 20:40:17 -0400

    Configure django-redis in settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -99,6 +99,20 @@ DATABASES = {
     }
 }
 
+#Redis config
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
