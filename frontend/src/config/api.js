65c090b07f8181b41784517ee09ba435541f2aec
liam-armstrong <liam.armstrong124@gmail.com>
2019-08-26 13:37:59 -0400

    removed localhost, added .env variable for external IP config

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,4 @@
-export const URL = "http://localhost:8000";
+const baseIP = process.env.IP
+export const URL = "http://" + baseIP + ":8000";
 export const LOGIN = "/api-token-auth/";
 export const SECTIONS = "/sections/"
