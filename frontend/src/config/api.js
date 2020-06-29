c906ef09bd2dc621160b567183080034e09fa8c4
Liam Armstrong <liam.armstrong124@gmail.com>
2020-06-29 00:29:27 -0700

    Fix remote api url pathing

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,3 @@
-export const URL = "http://localhost:8000";
+export const URL = "https://" + process.env.baseUrl +"/api";
 export const LOGIN = "/api-token-auth/";
 export const SECTIONS = "/sections/"
